1804dafbdca132d814d35775a5101f1ace35bfc6
Max Bay <cwang167@my.centennialcollege.ca>
2020-11-13 22:46:30 -0500

    7th Update
    Tournament CRUD is ready, but need to check robust.

--- routes/users.js
+++ routes/users.js
@@ -3,8 +3,21 @@ router = express.Router()
 const userCtrl = require('../controllers/userCtrl')
 
 /*GET /users/ show dashboard*/
-router.get('/', userCtrl.showDashboard)
+// router.get('/', userCtrl.showDashboard)
 
-router.get('/list', userCtrl.showMatchList)
+/*GET /users/list  show tournaments list*/
+router.get('/', userCtrl.showMatchList)
+
+/*POST add tournament to db*/
+router.post('/addTournament', userCtrl.addTournament)
+
+/*GET /users/modifyTournament show modify tournament page*/
+router.get('/modifyTournament/:_Id', userCtrl.showModifyTournament)
+
+/*GET /users/modifyTournament do modify tournament page*/
+router.post('/modifyTournament/', userCtrl.doModifyTournament)
+
+/*GET /users/deleteTournament  delete selected tournament*/
+router.get('/deleteTournament/:_Id', userCtrl.deleteTournament)
 
 module.exports = router;
